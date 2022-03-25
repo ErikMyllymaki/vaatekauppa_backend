2c56876f97fb4d9ffba33ba5a1a357d9703f4012
ErikMyllymaki <erik.myl01@gmail.com>
2022-03-25 13:29:35 +0200

    getshoes

--- products/getshoes.php
+++ products/getshoes.php
@@ -1,3 +1,12 @@
 <?php
-require_once 'inc/headers.php';
-require_once 'inc/functions.php';
+require_once '../inc/headers.php';
+require_once '../inc/functions.php';
+
+
+try {
+    $db = openDb();
+    selectAsJson($db, 'select * from tuote where kategoria_id = 3');
+}
+catch (PDOException $pdoex){
+    returnError($pdoex);
+}
