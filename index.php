36241b498b8889afbab814008cd58c4184d34455
ErikMyllymaki <erik.myl01@gmail.com>
2022-03-25 11:18:20 +0200

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 require_once 'inc/header.php';
-require_once "inc/functions.php";
+require_once 'inc/functions.php';
+
+try {
+    $db = openDb();
+    selectAsJson($db, 'select * from category');
+}
+catch (PDOException $pdoex){
+    returnError($pdoex);
+}
