33d1276c61de4c8275d3b74aaea7abae10a56f15
Eveliina Kaarivaara <eveliina.kaarivaara@gmail.com>
2022-03-25 13:24:43 +0200

    korjattu kirjotusvirhe

--- products/getproducts.php
+++ products/getproducts.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'inc/headers.php';
-require_once 'inc/functions.php';
+require_once '../inc/headers.php';
+require_once '../inc/functions.php';
 
 $uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
 $parameters = explode('/',$uri);
@@ -11,7 +11,7 @@ try {
     $db = openDb();
     $sql = "select * from category where id = $category_id";
     $query = $db->query($sql);
-    $category = $squery -> fetch(PDO::FETCH_ASSOC);
+    $category = $query -> fetch(PDO::FETCH_ASSOC);
 
     $sql = "select * from product where category_id = $category_id";
     $query = $db->query($sql);
