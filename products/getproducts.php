b88b2c996f134a0f89183aea5a62e3a038dfb717
Jenni Lohi <jennilohi@hotmail.com>
2022-03-25 13:11:57 +0200

    getproducts koodattu

--- products/getproducts.php
+++ products/getproducts.php
@@ -2,3 +2,27 @@
 
 require_once 'inc/headers.php';
 require_once 'inc/functions.php';
+
+$uri = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO'), PHP_URL_PATH);
+$parameters = explode('/',$uri);
+$category_id = $parameters[1];
+
+try {
+    $db = openDb();
+    $sql = "select * from category where id = $category_id";
+    $query = $db->query($sql);
+    $category = $squery -> fetch(PDO::FETCH_ASSOC);
+
+    $sql = "select * from product where category_id = $category_id";
+    $query = $db->query($sql);
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    header('HTTP/1.1 200 OK');
+    echo json_encode(array(
+        "category" => $category['name'],
+        "products" => $products
+    ));
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
