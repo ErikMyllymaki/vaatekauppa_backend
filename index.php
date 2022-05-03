6f34564f6d106911aadf3c50bcad161cb71cd48b
ErikMyllymaki <erik.myl01@gmail.com>
2022-05-03 11:51:22 +0300

    turha kommentti pois index.php:sta

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
 require_once 'inc/headers.php';
 require_once 'inc/functions.php';
-
-// try {
-    
-//     $db = openDb();
-//     selectAsJson($db, 'select * from kategoria');
-// }
-// catch (PDOException $pdoex){
-//     returnError($pdoex);
-// }
